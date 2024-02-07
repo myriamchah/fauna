a5a112ec2edb0087d6e81aaa906ff2fc01bde505
myriam chahlafi <myriam.chah@gmail.com>
2024-02-07 17:42:40 +0700

    Add dropdown menu for Owner actions + add chakra ui's icons

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { anton } from "../layout";
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import {
+  AddIcon,
+  UnlockIcon,
+  LockIcon,
+  StarIcon,
+  TriangleDownIcon,
+} from "@chakra-ui/icons";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import { useUserContext } from "../contexts/userContext";
+import { useContractContext } from "../contexts/contractContext";
 
 const Header = () => {
   const { isConnected } = useAccount();
   const { isOwner, isDonator } = useUserContext();
+  const { startVotes, endVotes, sendFunds } = useContractContext();
 
   return (
     <Flex
@@ -29,6 +45,26 @@ const Header = () => {
           <Text>{isOwner && "Owner"}</Text>
           <Text>{isDonator && "Donator"}</Text>
         </>
+      )}
+      {isConnected && isOwner && (
+        <Menu>
+          <MenuButton fontWeight="600" mr="2rem ">
+            Owner actions
+            <TriangleDownIcon boxSize="0.5em" ml="0.25rem" />
+          </MenuButton>
+          <MenuList color="#24280a">
+            <MenuItem icon={<AddIcon />}>Add a project</MenuItem>
+            <MenuItem icon={<UnlockIcon />} onClick={startVotes}>
+              Open Votes
+            </MenuItem>
+            <MenuItem icon={<LockIcon />} onClick={endVotes}>
+              Close Votes
+            </MenuItem>
+            <MenuItem icon={<StarIcon />} onClick={sendFunds}>
+              Send Funds
+            </MenuItem>
+          </MenuList>
+        </Menu>
       )}
       <ConnectButton />
     </Flex>
