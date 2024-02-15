2dcfefd4b20f7fe2bac621d6402653a6b6b7a337
myriam chahlafi <myriam.chah@gmail.com>
2024-02-15 11:47:22 +0700

    Add config for sepolia

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { vars } = require("hardhat/config");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.23",
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: vars.get("SEPOLIA_RPC_URL"),
+      accounts: [vars.get("SEPOLIA_WALLET_PK")],
+    },
+  },
 };
